9625fca59f650475747a2fe8ce8e2d07664202e9
EDGE-Marketing <158225052+EDGE-Marketing@users.noreply.github.com>
2024-06-24 13:30:50 -0600

    Update Sidebar.vue

--- components/app/Sidebar.vue
+++ components/app/Sidebar.vue
@@ -36,7 +36,7 @@ const { menus } = await useMenu()
       </div>
       <div>
         <div class="border-t border-gray-400/30 my-4" />
-        <div class="flex justify-center gap-4 collapsed:!flex-direction: column collapsed:!align-items: center">
+        <div class="flex row wrap justify-center gap-4">
           
           <NuxtLink v-for="item of socials" :key="item.title" :to="item._path" target="_blank">
             <Icon class="w-6 h-6 !text-gray-900 dark:!text-white padding:2px" :name="item.icon" />
