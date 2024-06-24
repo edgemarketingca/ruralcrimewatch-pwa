01e0ea3fb08ddd4de310b7c88c4e9f4f4d5e9641
EDGE-Marketing <158225052+EDGE-Marketing@users.noreply.github.com>
2024-06-24 12:59:24 -0600

    Update Sidebar.vue
    testing updated socials display

--- components/app/Sidebar.vue
+++ components/app/Sidebar.vue
@@ -36,6 +36,17 @@ const { menus } = await useMenu()
       </div>
       <div>
         <div class="border-t border-gray-400/30 my-4" />
+         <NuxtLink v-for="item of socials" :key="item.title" :to="item._path" target="_blank">
+          <Icon class="w-6 h-6 text-white" :name="item.icon" />
+        </NuxtLink>
+        <AppColorMode />
+        <button
+          class="text-white"
+          @click="open = !open"
+        >
+          <Icon class="w-6 h-6" :name="open ? 'i-ph-x' : 'i-ph-list'" />
+        </button>
+        <!--
         <AppNav class="grid items-center justify-center gap-2 place-items-center" :class="collapsed ? 'grid-cols-1' : 'grid-cols-5'" :items="socials" :collapsed="true">
           <li>
             <AppColorMode class="w-full flex items-center p-2 !text-gray-900 dark:!text-white rounded-lg hover:bg-yellow-400 hover:text-black dark:hover:!text-black transition" :class="{ 'justify-center': collapsed }">
@@ -45,6 +56,7 @@ const { menus } = await useMenu()
             </AppColorMode>
           </li>
         </AppNav>
+        -->
       </div>
     </div>
   </aside>
