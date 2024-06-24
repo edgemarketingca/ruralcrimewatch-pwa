173671f23f88806045e726ebffea3a4b1b9c1d0e
EDGE-Marketing <158225052+EDGE-Marketing@users.noreply.github.com>
2024-06-24 12:56:33 -0600

    Update socials.ts

--- constants/socials.ts
+++ constants/socials.ts
@@ -4,21 +4,25 @@ export const socials: Menu[] = [
   {
     title: 'Youtube',
     icon: 'ph:youtube-logo-duotone',
+    target: '_blank',
     _path: 'https://www.youtube.com/channel/UCYDRG7rdEsKj2LJGgDOhmvw',
   },
   {
     title: 'Instagram',
     icon: 'ph:instagram-logo-duotone',
+    target: '_blank',
     _path: 'https://www.instagram.com/ab_crimewatch/',
   },
   {
     title: 'Facebook',
     icon: 'ph:facebook-logo-duotone',
+    target: '_blank',
     _path: 'https://www.facebook.com/albertaprovincialruralcrimewatch',
   },
   {
     title: 'Website',
     icon: 'ph:globe-duotone',
+    target: '_blank',
     _path: 'https://www.ruralcrimewatch.ab.ca',
   },
 ]
