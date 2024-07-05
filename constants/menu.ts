7b9f46180d7a4197ba7e5c0e2055565c9ae9d6e1
EDGE-Marketing <158225052+EDGE-Marketing@users.noreply.github.com>
2024-07-05 14:55:02 -0600

    Update menu.ts

--- constants/menu.ts
+++ constants/menu.ts
@@ -4,11 +4,13 @@ export const menu: Menu[] = [
   {
     title: 'Home',
     icon: 'ph:house-duotone',
+    target: 'self',
     _path: '/',
   },
   {
     title: 'RCMP Crime Map',
     icon: 'ph:map-trifold-duotone',
+    target: 'self',
     _path: '/map',
   },
 ]
@@ -17,16 +19,19 @@ export const mobileMenu: Menu[] = [
   {
     title: '', //home
     icon: 'ph:house-duotone',
+    target: 'self',
     _path: '/',
   },
   {
     title: 'Crime Map',
     icon: 'ph:map-trifold-duotone',
+    target: 'self',
     _path: '/map',
   },
   {
     title: 'Newsletter',
     icon: 'i-ic-outline-mark-email-unread',
+    target: '_blank',
     _path: 'https://www.ruralcrimewatch.ab.ca/join-our-newsletter',
   },
 ]
