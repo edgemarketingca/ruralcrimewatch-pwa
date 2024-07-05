b2853cc7a5ec022cada517dfe69f005307eef29e
EDGE-Marketing <158225052+EDGE-Marketing@users.noreply.github.com>
2024-07-05 12:03:25 -0600

    Update Sidebar.vue

--- components/app/Sidebar.vue
+++ components/app/Sidebar.vue
@@ -44,7 +44,7 @@ const { menus } = await useMenu()
           </NuxtLink>
           -->
           
-          <b-button v-for="item of socials" :type="button" :data-href="item._path" :label="item.title" :click="window.open(this.getAttribute('data-href'),'item.target')">
+          <b-button v-for="item of socials" :type="button" :data-href="item._path" :label="item.title">
             <Icon class="w-6 h-6 !text-gray-900 dark:!text-white padding:2px" :name="item.icon" />
           </b-button>
             
