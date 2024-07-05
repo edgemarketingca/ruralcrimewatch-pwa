2affb13074d983783a10cdf7bc9406d8063b6063
EDGE-Marketing <158225052+EDGE-Marketing@users.noreply.github.com>
2024-07-05 11:48:04 -0600

    Update Sidebar.vue
    testing updated social button links

--- components/app/Sidebar.vue
+++ components/app/Sidebar.vue
@@ -44,7 +44,7 @@ const { menus } = await useMenu()
           </NuxtLink>
           -->
           
-          <b-button :type="button" v-for="item of socials" :label="item.title" :to="item._path" :target="item.target">
+          <b-button v-for="item of socials" type="button" data-href="item._path" :label="item.title" v-on:click="window.open(this.getAttribute('data-href'),'item.target'); return false;">
             <Icon class="w-6 h-6 !text-gray-900 dark:!text-white padding:2px" :name="item.icon" />
           </b-button>
             
