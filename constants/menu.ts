36bd5babe58c1de3b8842f5db142d49777543b64
EDGE-Marketing <158225052+EDGE-Marketing@users.noreply.github.com>
2024-07-10 09:37:49 -0600

    Update menu.ts
    Added meta title options for links

--- constants/menu.ts
+++ constants/menu.ts
@@ -3,12 +3,14 @@ import type { Menu } from "~/types";
 export const menu: Menu[] = [
   {
     title: 'Home',
+    metatitle: 'Home',
     icon: 'ph:house-duotone',
     target: 'self',
     _path: '/',
   },
   {
     title: 'RCMP Crime Map',
+    metatitle: 'RCMP Crime Map',
     icon: 'ph:map-trifold-duotone',
     target: 'self',
     _path: '/map',
@@ -18,18 +20,21 @@ export const menu: Menu[] = [
 export const mobileMenu: Menu[] = [
   {
     title: '', //home
+    metatitle: 'Home',
     icon: 'ph:house-duotone',
     target: 'self',
     _path: '/',
   },
   {
     title: 'Crime Map',
+    metatitle: 'Crime Map',
     icon: 'ph:map-trifold-duotone',
     target: 'self',
     _path: '/map',
   },
   {
     title: 'Newsletter',
+    metatitle: 'Newsletter',
     icon: 'i-ic-outline-mark-email-unread',
     target: '_blank',
     _path: 'https://www.ruralcrimewatch.ab.ca/join-our-newsletter',
