ac7b4cd254c4d09aa1745f47283da11e46b733d5
EDGE-Marketing <158225052+EDGE-Marketing@users.noreply.github.com>
2024-07-10 09:40:20 -0600

    Update BottomBar.vue
    Updated to allow custom link meta titles

--- components/app/BottomBar.vue
+++ components/app/BottomBar.vue
@@ -6,7 +6,7 @@ import { mobileMenu } from '~/constants'
   <ul class="fixed bottom-0 right-0 left-0 z-50 bg-black/40 backdrop-blur-lg flex justify-between sm:hidden" style="font-size: 0.9rem;">
     <li v-for="item, index of mobileMenu" :key="item._path" class="w-auto" :class="{ ' w-full border-l border-transparent hover:border-gray-200/20': index }">
       <NuxtLink
-        :title="item.title"
+        :title="item.metatitle"
         :to="item._path"
         active-class="bg-yellow-400 !text-black"
         class="flex items-center p-4 text-gray-900 dark:text-white hover:bg-gray-100 dark:hover:bg-yellow-400 dark:hover:text-black hover:text-black transition"
