bdd01bb0cc8125968a167347deb2b7dd8179c668
EDGE-Marketing <158225052+EDGE-Marketing@users.noreply.github.com>
2024-07-18 11:50:30 -0600

    Update news.vue

--- pages/news.vue
+++ pages/news.vue
@@ -3,25 +3,93 @@ import { version } from '~/package.json'
 </script>
 
 <template>
-  <main class="dark:text-white">
+  <main class="list-page text-black dark:text-white">
     
-    <h2 class="text-3xl text-primary">
-      News & Updates from the APRCWA
-    </h2>
-    <div class="flex flex-col gap-8 my-4 pl-4">
-      
-    <!-- Goal: display full list of news articles, with pagination -->
-      
+    <div class="sticky top-4 flex justify-between items-center bg-primary p-2 rounded-lg mb-6">
+      <NuxtLink title="Home" class="p-2" to="/">
+        <Icon class="w-5 h-5" name="ph:arrow-left" />
+      </NuxtLink>
+      <div>
+        <span class="font-bold text-xl text-black">
+          News & Updates from the APRCWA
+        </span>
+      </div>
+      <div />
     </div>
-    
+
+    <section class="grid grid-cols-12 gap-6">
+       
+      <!-- Goal: display full list of news articles, with pagination -->
+      
+    </section>
+        
     <hr />
 
-    <h2 class="text-3xl text-primary">
-      Signup for our Newsletter: 
-    </h2>
-    <NuxtLink to="https://www.ruralcrimewatch.ab.ca/join-our-newsletter" target="_blank" class="bg-primary p-2 rounded-lg opacity-80 group-hover:opacity-100 transition-all !text-black">
-      Join Today!
-    </NuxtLink>
+    <section class="grid grid-cols-12 gap-6">
        
+      <h2 class="text-3xl text-primary">
+        Signup for our Newsletter: 
+      </h2>
+      <NuxtLink to="https://www.ruralcrimewatch.ab.ca/join-our-newsletter" target="_blank" class="bg-primary p-2 rounded-lg opacity-80 group-hover:opacity-100 transition-all !text-black">
+        Join Today!
+      </NuxtLink>
+      
+    </section>
+  
   </main>
 </template>
+
+<style lang="scss">
+.list-page {
+  .toc {
+    h2, h3 {
+      margin-bottom: 0;
+      font-size: 14px;
+    }
+  }
+  h1 {
+    font-size: 2.5rem;
+    font-weight: 700;
+    margin-bottom: 1rem;
+  }
+  h2 {
+    font-size: 2rem;
+    font-weight: 600;
+    margin-bottom: 1rem;
+  }
+  h3 {
+    font-size: 1.75rem;
+    font-weight: 500;
+    margin-bottom: 1rem;
+  }
+  h4 {
+    font-size: 1.5rem;
+    font-weight: 500;
+    margin-bottom: 1rem;
+  }
+  h5 {
+    font-size: 1.25rem;
+    font-weight: 500;
+    margin-bottom: 1rem;
+  }
+  h6 {
+    font-size: 1rem;
+    font-weight: 500;
+    margin-bottom: 1rem;
+  }
+  p + p {
+    margin-bottom: 1rem;
+  }
+  p {
+    font-size: 1rem;
+    font-weight: 400;
+  }
+  ul {
+    list-style: disc;
+    padding-left: 1rem;
+    li {
+      margin-bottom: 0.5rem;
+    }
+  }  
+}
+</style>
