75576c50f00b21736b99c4e010c020a4f8c2bb37
EDGE-Marketing <158225052+EDGE-Marketing@users.noreply.github.com>
2024-07-18 11:39:37 -0600

    Update news.vue

--- pages/news.vue
+++ pages/news.vue
@@ -1,1 +1,29 @@
-/* Goal: display full list of news articles, with pagination */
+<script setup lang="ts">
+import { version } from '~/package.json'
+</script>
+
+<template>
+  <main class="dark:text-white">
+    
+    <h2 class="text-3xl text-primary">
+      News & Updates from the APRCWA
+    </h2>
+    <div class="flex flex-col gap-8 my-4 pl-4">
+      
+    <!-- Goal: display full list of news articles, with pagination -->
+      
+    </div>
+    
+    <hr />
+
+    <h2 class="text-3xl text-primary">
+      Signup for our Newsletter: 
+    </h2>
+    <NuxtLink
+      class="bg-primary p-2 rounded-lg opacity-80 group-hover:opacity-100 transition-all !text-black"
+      :to="https://www.ruralcrimewatch.ab.ca/join-our-newsletter">
+      Join Today!
+    </NuxtLink>
+       
+  </main>
+</template>
