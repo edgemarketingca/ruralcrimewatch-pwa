138ba37b124966da1cc6a692710f21e868363bcb
EDGE-Marketing <158225052+EDGE-Marketing@users.noreply.github.com>
2024-07-18 08:56:40 -0600

    Update [...slug].vue
    Fix - content colour on light/dark switch

--- pages/news/[...slug].vue
+++ pages/news/[...slug].vue
@@ -13,7 +13,7 @@ function getFile(file: string) {
 </script>
 
 <template>
-  <div class="text-white content-page">
+  <div class="text-black dark:text-white content-page">
     <div v-for="content, index of article?.areas.data" :key="index">
       <div class="mt-4" v-for="block of content.blocks.data" :key="block.id">
         <div v-html="block.value.content" />
