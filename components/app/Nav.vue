b33cd4f55835941ef3b2d92a3d806cc0ceffbdc9
EDGE-Marketing <158225052+EDGE-Marketing@users.noreply.github.com>
2024-08-01 09:54:00 -0600

    Update Nav.vue

--- components/app/Nav.vue
+++ components/app/Nav.vue
@@ -19,7 +19,7 @@ const model = useVModel(props, 'open')
         :title="item.title"
         :to="item._path"
         :class="{ 'justify-center': collapsed }"
-        active-class="bg-yellow-400"  
+        active-class="bg-yellow-400 !text-black"  
         class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-yellow-400 hover:text-black dark:hover:text-black transition"
         @click="model = false"
       >
