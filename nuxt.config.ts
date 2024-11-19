fcdc386b105972b3eb25f28c0ce3b79901271443
EDGE-Marketing <158225052+EDGE-Marketing@users.noreply.github.com>
2024-11-19 15:04:43 -0700

    Update nuxt.config.ts
    Add custom stylesheet

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,7 @@ export default defineNuxtConfig({
       link: [
         { rel: 'favicon', href: '/favicon.ico' },
         { rel: 'manifest', href: '/manifest.json' },
+        { rel: 'stylesheet', href: '/appstyles.css' },
       ],
       
     },
