8fbbab281b8052964c865a5b97929a91c4ce467d
Nick Kleymenov <nick@kleymenov.pro>
2024-12-10 16:55:19 +0300

    PWA Content Display Update - relative to absolute URL conversion for article images.

--- server/api/pages/[page].post.ts
+++ server/api/pages/[page].post.ts
@@ -1,24 +1,30 @@
-import { getToken } from "~/server/utils/token";
-import { parse } from "node-html-parser";
+import {getToken} from "~/server/utils/token";
+import {parse} from "node-html-parser";
 
 export default defineEventHandler(async (event) => {
-  const page = getRouterParam(event, 'page');
-  const token  = await getToken();
+    const page = getRouterParam(event, 'page');
+    const token = await getToken();
 
-  const res =  await $fetch(`https://www.ruralcrimewatch.ab.ca/ccm/api/1.0/pages/${page}?includes=custom_attributes,areas,files,content`, {
-    headers: {
-      'Authorization': `Bearer ${token}`,
-    },
-  })
+    const res = await $fetch(`https://www.ruralcrimewatch.ab.ca/ccm/api/1.0/pages/${page}?includes=custom_attributes,areas,files,content`, {
+        headers: {
+            'Authorization': `Bearer ${token}`,
+        },
+    })
 
-  const rawContent = res?.data?.content?.data?.raw;
-  if (rawContent) {
-    const htmlDoc = parse(rawContent);
-    const htmlContent = htmlDoc.querySelector('#cntwrap').toString();
-    res.data.htmlContent = htmlContent;
-  }
+    const rawContent = res?.data?.content?.data?.raw;
+    if (rawContent) {
+        const htmlDoc = parse(rawContent);
+        const images = htmlDoc.querySelectorAll('img');
+        const baseUrl = 'https://www.ruralcrimewatch.ab.ca';
+        images.map((image) => {
+            const imgSrc = image.getAttribute('src');
+            if (!imgSrc.includes('https') || !imgSrc.includes('http')) {
+                image.setAttribute('src', baseUrl + imgSrc)
+            }
+        });
 
-  console.log(res.data.htmlContent);
+        res.data.htmlContent = htmlDoc.querySelector('#cntwrap').toString();
+    }
 
-  return res?.data
+    return res?.data
 })
