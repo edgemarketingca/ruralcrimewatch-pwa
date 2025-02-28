4c5385e47fcb0f9389f8c90a9635d9909f31f48d
EDGE-Marketing <158225052+EDGE-Marketing@users.noreply.github.com>
2025-02-28 08:22:35 -0700

    Update nuxt.config.ts
    Analytics Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,41 +17,7 @@ export default defineNuxtConfig({
     keepalive: true,
   },
   
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@nuxthq/studio', '@nuxt/content', '@vueuse/nuxt', 'nuxt-icon', 'nuxt-gtag'],
-
-  /*
-  security: {
-    nonce: true, // Enables HTML nonce support in SSR mode
-    ssg: {
-      meta: true, // Enables CSP as a meta tag in SSG mode
-      hashScripts: true, // Enables CSP hash support for scripts in SSG mode
-      hashStyles: false // Disables CSP hash support for styles in SSG mode (recommended)
-    },
-    headers: {
-      contentSecurityPolicy: {
-        'script-src': [
-          "'self'",  // Fallback value, will be ignored by most modern browsers (level 3)
-          "https:", // Fallback value, will be ignored by most modern browsers (level 3)
-          "'unsafe-inline'", // Fallback value, will be ignored by almost any browser (level 2)
-          "'strict-dynamic'", // Strict CSP via 'strict-dynamic', supported by most modern browsers (level 3)
-          "'nonce-{{nonce}}'" // Enables CSP nonce support for scripts in SSR mode, supported by almost any browser (level 2)
-        ],
-        'style-src': [
-          "'self'", // Enables loading of stylesheets hosted on same origin
-          "https:", // For increased security, replace by the specific hosting domain or file name of your external stylesheets
-          "'unsafe-inline'" // Recommended default for most Nuxt apps
-        ],
-        'base-uri': ["'none'"],
-        'img-src': ["'self'", "data:https://www.ruralcrimewatch.ab.ca"], // Add relevant https://... sources if you load images from external sources 
-        'font-src': ["'self'", "https:"], // (, "data:") For increased security, replace by the specific sources for fonts
-        'object-src': ["'none'"],
-        'script-src-attr': ["'none'"],
-        'upgrade-insecure-requests': true
-      }
-    },
-    sri: true
-  },
-  */
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@nuxthq/studio', '@nuxt/content', '@vueuse/nuxt', 'nuxt-icon', 'nuxt-gtag', '@nuxtjs/google-analytics'],
   
   tailwindcss: {
     configPath: 'tailwind.config.ts',
@@ -79,7 +45,16 @@ export default defineNuxtConfig({
   gtag: {
     id: 'G-VPRLB74ECH',
   },
-
+  googleAnalytics: {
+    id: 'G-VPRLB74ECH', 
+    dev: testingAnalytics ? true : process.env.NODE_ENV !== 'production',
+    debug: testingAnalytics
+      ? {
+        enabled: true,
+        sendHitTask: true
+      }
+      : {}
+  }
   loadingIndicator: {
     name: '~/composables/loading.html',
     img: '/icons/192.png'
